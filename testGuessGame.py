549836ebb3ad299f000f788866098d1b1d20065c
Kweronda <kwerondaa@gmail.com>
2019-01-25 16:02:20 +0300

    added comment to test file

--- testGuessGame.py
+++ testGuessGame.py
@@ -5,7 +5,7 @@ import guessgame
 
 class TestGuess(unittest.TestCase):
     def test_if_guess_true(self):
-        self.assertEqual(guessgame.guessGame(), True)
+        self.assertEqual(guessgame.guessGame(), True) #since i didn't add any arguments in the function back in the program file, how do I test it now?
         self.assertEqual(guessgame.guessGame(), False )
 
 if __name__ == "__main__":
